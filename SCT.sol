1aefe5b3fc62d83a47a2343066e886b4958cef40
denispeplin <denis.peplin@gmail.com>
2017-11-17 10:33:13 +0300

    Inherit contract from MintableToken.

--- SCT.sol
+++ SCT.sol
@@ -1,76 +1,9 @@
 pragma solidity ^0.4.18;
 
-contract Ownable {
-  address owner;
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/token/MintableToken.sol";
 
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  function transferOwnership(address newOwner) public onlyOwner {
-    owner = newOwner;
-  }
-}
-
-contract SimpleCoinToken is Ownable {
+contract SimpleCoinToken is MintableToken {
   string public constant name = "Simple Coin Token";
   string public constant symbol = "SCT";
   uint32 public constant decimals = 18;
-
-  uint public totalSupply = 0;
-
-  mapping (address => uint) balances;
-
-  mapping (address => mapping (address => uint)) allowed;
-
-  function mint(address _to, uint _value) public onlyOwner {
-    assert(totalSupply + _value >= totalSupply && balances[_to] + _value >= balances[_to]);
-    totalSupply += _value;
-    balances[_to] += _value;
-  }
-
-  function balanceOf(address _owner) constant public returns (uint balance) {
-    return balances[_owner];
-  }
-
-  function transfer(address _to, uint _value) public returns (bool success) {
-    if (balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-      balances[msg.sender] -= _value;
-      balances[_to] += _value;
-      Transfer(msg.sender, _to, _value);
-      return true;
-    }
-    return false;
-  }
-
-  function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
-    if (allowance(_from, msg.sender) >= _value &&
-        balances[_from] >= _value &&
-        balances[_to] + _value >= balances[_to]) {
-      balances[_from] -= _value;
-      balances[_to] += _value;
-      Transfer(_from, _to, _value);
-      return true;
-    }
-    return false;
-  }
-
-  function approve(address _spender, uint _value) public returns (bool success) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) public constant returns (uint remaining) {
-    return allowed[_owner][_spender];
-  }
-
-  event Transfer(address indexed _from, address indexed _to, uint _value);
-
-  event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
