e16efba2492d54917118cb7d207901775ce5ba6a
denispeplin <denis.peplin@gmail.com>
2017-12-01 09:31:44 +0300

    Add Crowdsale contract.

--- SCT.sol
+++ SCT.sol
@@ -7,3 +7,24 @@ contract SimpleCoinToken is MintableToken {
   string public constant symbol = "SCT";
   uint32 public constant decimals = 18;
 }
+
+contract Crowdsale {
+
+  address owner;
+
+  SimpleCoinToken public token = new SimpleCoinToken();
+
+  uint start = 1512086400;
+
+  uint period = 28;
+
+  function Crowdsale() public {
+    owner = msg.sender;
+  }
+
+  function() external payable {
+    require(now > start && now < start + period*24*60*60);
+    owner.transfer(msg.value);
+    token.mint(msg.sender, msg.value);
+  }
+}
